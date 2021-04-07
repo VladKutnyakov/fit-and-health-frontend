23dd12dbe3e5e8b0ebc81dc939f3e9ecfb3805c2
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-04-07 09:14:53 +0400

    update training programs book layout

--- components/trainingProgramsBook/FoundProgram.vue
+++ components/trainingProgramsBook/FoundProgram.vue
@@ -1,45 +1,52 @@
 <template>
   <div class="found-program">
     <div class="found-program__overview">
-      <div class="found-program__main-info">
-        <p class="training-description__title">Круговая жиросжигающая тренировка "BATMAN"</p>
+      <div class="overview__image">
+        <img class="image" src="https://cdn.imgbb.ru/community/7/74658/201602/2e5bbfb866325797fc99c79021a9b829.jpg" alt />
+      </div>
 
-        <div class="main-info__training-description">
-          <div class="training-description__img-wrapper">
-            <img
-              class="training-description__img"
-              src="https://cdn.imgbb.ru/community/7/74658/201602/2e5bbfb866325797fc99c79021a9b829.jpg"
-              alt
-            />
+      <div class="overview____main-info">
+        <p class="title">Круговая жиросжигающая тренировка "BATMAN"</p>
+
+        <div class="additional-info">
+          <div class="additional-info__element">
+            <i class="ti-timer element__icon"></i>
+            <p class="element__amount">4</p>
+            <p class="element__text">тренировочных дня</p>
           </div>
-
-          <div class="training-description__overview">
-            <div class="training-description__statistics">
-              <div class="statistics__element">
-                <i class="ti-timer"></i>
-                <p class="element__value">4</p>
-                <p class="element__title">тренировочных дня</p>
-              </div>
-              <div class="statistics__element">
-                <i class="ti-check-box"></i>
-                <p class="element__value">27</p>
-                <p class="element__title">завершенных тренировок</p>
-              </div>
-            </div>
-
-            <div class="training-description__short-text-overview">
-              <p class="short-text-overview__block-title">Описание</p>
-              <p
-                class="short-text-overview__text"
-              >Короткое описание тренировочной программы на 100-200 символов остальное будет обрезано. блок со скроллом используется.</p>
-              <br />
-              <p>НАЗВАНИЕ СДЕЛАТЬ СВЕРХУ НАД КАРТИНКОЙИ СЛОЖНОСТЬ НА ВСЮ ДЛИНУ КАК И НАЗВАНИЕ</p>
-            </div>
+          <div class="additional-info__element">
+            <i class="ti-check-box element__icon"></i>
+            <p class="element__amount">27</p>
+            <p class="element__text">завершенных тренировок</p>
           </div>
         </div>
 
-        <div class="training-description__training-skill">
-          <p class="training-skill__text">Сложность тренировочной программы:</p>
+        <div class="description">
+          <p class="description__title">Описание</p>
+          <p class="description__text">Короткое описание тренировочной программы</p>
+        </div>
+
+        <div class="added-marks">
+          <div class="mark">
+            <p class="mark__text">Низкоуглеводное</p>
+            <i class="ti-close mark__icon"></i>
+          </div>
+          <div class="mark">
+            <p class="mark__text">Веганское</p>
+            <i class="ti-close mark__icon"></i>
+          </div>
+          <div class="mark">
+            <p class="mark__text">Без ГМО</p>
+            <i class="ti-close mark__icon"></i>
+          </div>
+          <div class="mark">
+            <p class="mark__text">Для набора массы</p>
+            <i class="ti-close mark__icon"></i>
+          </div>
+        </div>
+
+        <div class="training-skill">
+          <p class="training-skill__block-title">Сложность тренировочной программы:</p>
           <div class="training-skill__level-view">
             <i class="ti-crown level-view__icon level-view__icon--active"></i>
             <i class="ti-crown level-view__icon level-view__icon--active"></i>
@@ -50,49 +57,33 @@
         </div>
       </div>
 
-      <div class="found-program__chart-and-marks">
-        <div class="chart">
-          <p class="chart__block-title">Акцент тренировок</p>
-          <img
-            src="https://image.freepik.com/free-vector/circle-chart-infographic-template-with-options_93208-410.jpg"
-            alt
-          />
-          <p>Кардио: 30%</p>
-          <p>Сила: 20%</p>
-          <p>Выносливость: 38%</p>
-          <p>Гибкость: 12%</p>
-        </div>
-
-        <div class="added-marks">
-          <p class="added-marks__block-title">Добавленные отметки</p>
-          <div class="added-marks__item">Кардио</div>
-          <div class="added-marks__item">Жиросжигающая</div>
-          <div class="added-marks__item">Круговая</div>
-          <div class="added-marks__item">На все группы мышц</div>
-        </div>
+      <div class="overview__chart">
+        <p class="chart__block-title">Акцент тренировок</p>
+        <img
+          src="https://image.freepik.com/free-vector/circle-chart-infographic-template-with-options_93208-410.jpg"
+          alt
+        />
+        <p>Кардио: 30%</p>
+        <p>Сила: 20%</p>
+        <p>Выносливость: 38%</p>
+        <p>Гибкость: 12%</p>
       </div>
-    </div>
-    <div class="found-program__action-btns">
-      <app-button uppercase>Удалить программу</app-button>
 
-      <nuxt-link to="training-programs/program-123" class="open-program-btn ml-auto">
-        <app-button uppercase>Подробнее</app-button>
-      </nuxt-link>
-
-      <app-button uppercase class="ml-5">Сделать основной</app-button>
+      <div class="overview__actions">
+        <i class="ti-control-play action-btn"></i>
+        <nuxt-link to="training-programs/program-123" class="open-program-btn ml-auto">
+          <i class="ti-search action-btn"></i>
+        </nuxt-link>
+        <i class="ti-check-box action-btn"></i>
+        <i class="ti-trash action-btn"></i>
+      </div>
     </div>
 
   </div>
 </template>
 
 <script>
-import AppButton from '@/components/basic/AppButton'
-
-export default {
-  components: {
-    AppButton
-  }
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
@@ -100,94 +91,121 @@ export default {
 
 .found-program {
   // border: 1px solid red;
-  margin-bottom: 40px;
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
   .found-program__overview {
     display: flex;
-    padding: 10px;
     width: 100%;
-    transition: $tr-02;
-    background: $hiddenBlockBG;
+    background: $white;
+    border: 1px solid $blockBorder;
     border-radius: 6px;
-    box-shadow: $btnHoverShadow;
-    .found-program__main-info {
+    .overview__image {
+      padding: 10px;
+      .image {
+        width: 100%;
+        min-width: 250px;
+        max-width: 250px;
+        object-fit: cover;
+        object-position: center;
+        border: 1px solid $blockBorder;
+        border-radius: 6px;
+      }
+    }
+
+    .overview____main-info {
+      flex: 1 1 auto;
       display: flex;
       flex-direction: column;
-      // margin: 10px 0 10px 10px;
-      padding: 10px;
-      background: $white;
-      border: 1px solid $blockBorder;
-      border-radius: 6px;
-
-      .training-description__title {
+      padding: 10px 10px 10px 0;
+      .title {
         padding: 0 10px 10px 10px;
+        text-align: center;
         font-weight: 500;
-        border-bottom: 1px dashed $blockBorder;
+        border-bottom: 1px solid $blockBorder;
       }
-      .main-info__training-description {
-        // border: 1px solid red;
-        flex: 1 1 auto;
+
+      .additional-info {
         display: flex;
-        // margin-left: 10px;
-        .training-description__img-wrapper {
-          // border: 1px solid red;
-          margin-top: 10px;
-          margin-bottom: 10px;
-          width: 200px;
-          min-width: 200px;
-          max-width: 200px;
-          border: 1px solid $blockBorder;
-          border-radius: 6px;
-          overflow: hidden;
-          .training-description__img {
+        padding: 10px 0;
+        background: $white;
+        .additional-info__element {
+          flex: 1 1 auto;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border-right: 1px solid rgba(0, 0, 0, 0.2);
+          .element__icon {
+            font-size: 16px;
+          }
+
+          .element__amount {
+            margin-left: 10px;
+            font-size: 14px;
+            font-weight: 600;
+          }
+
+          .element__text {
+            margin-left: 5px;
+            font-size: 12px;
+            font-weight: 500;
+            white-space: nowrap;
           }
         }
-        .training-description__overview {
-          margin-left: 10px;
+        .additional-info__element:last-child {
+          border: none;
+        }
+      }
+
+      .description {
+        flex: 1 1 auto;
+        padding-top: 10px;
+        border-top: 1px solid $blockBorder;
+        .description__title {
+          text-align: center;
+          font-weight: 500;
+        }
+        .description__text {
+          padding: 10px;
+        }
+      }
+
+      .added-marks {
+        display: flex;
+        flex-wrap: wrap;
+        margin-top: 10px;
+        padding-top: 10px;
+        padding-bottom: 5px;
+        border-top: 1px solid $blockBorder;
+        .mark {
           display: flex;
-          flex-direction: column;
-          .training-description__statistics {
-            display: flex;
-            // margin-top: 10px;
-            padding: 10px;
-            border-bottom: 1px dashed $blockBorder;
-            .statistics__element {
-              // border: 1px solid red;
-              flex: 1 1 auto;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              .element__value {
-                margin-left: 10px;
-                font-size: 14px;
-                font-weight: 600;
-              }
-              .element__title {
-                margin-left: 5px;
-                font-size: 12px;
-                font-weight: 500;
-                white-space: nowrap;
-              }
-            }
+          align-items: center;
+          justify-content: space-between;
+          margin-right: 5px;
+          margin-bottom: 5px;
+          padding: 5px 10px;
+          background: $hiddenBlockBG;
+          border-radius: 6px;
+          .mark__text {
+            font-size: 14px;
+            white-space: nowrap;
           }
-          .training-description__short-text-overview {
-            padding: 10px;
-            flex: 1 1 auto;
-            display: flex;
-            flex-direction: column;
-            .short-text-overview__block-title {
-              align-self: center;
-              font-weight: 500;
-            }
+          .mark__icon {
+            margin-left: 10px;
+            font-size: 10px;
+            cursor: pointer;
           }
         }
       }
-      .training-description__training-skill {
+
+      .training-skill {
         display: flex;
         align-items: center;
         justify-content: space-between;
         padding: 10px 10px 0 10px;
-        border-top: 1px dashed rgba(0, 0, 0, 0.2);
-        .training-skill__text {
+        border-top: 1px solid $blockBorder;
+        .training-skill__block-title {
           font-size: 14px;
           font-weight: 500;
         }
@@ -203,55 +221,35 @@ export default {
       }
     }
 
-    .found-program__chart-and-marks {
-      display: flex;
-      .chart {
-        width: 230px;
-        min-width: 230px;
-        max-width: 230px;
-        margin-left: 5px;
-        padding: 10px;
-        background: $white;
-        border: 1px solid $blockBorder;
-        border-radius: 6px;
-        .chart__block-title {
-          margin-bottom: 10px;
-          padding: 0 10px 10px 10px;
-          text-align: center;
-          font-weight: 500;
-          border-bottom: 1px dashed rgba(0, 0, 0, 0.2);
-          white-space: nowrap;
-        }
-      }
-      .added-marks {
-        margin-left: 5px;
-        padding: 10px;
-        background: $white;
-        border: 1px solid $blockBorder;
-        border-radius: 6px;
-        .added-marks__block-title {
-          margin-bottom: 10px;
-          padding: 0 10px 10px 10px;
-          text-align: center;
-          font-weight: 500;
-          border-bottom: 1px dashed rgba(0, 0, 0, 0.2);
-          white-space: nowrap;
-        }
-        .added-marks__item {
-          // margin-bottom: 5px;
-          margin: 0 10px 5px 10px;
-          padding: 5px 10px;
-          background: $hiddenBlockBG;
-          border-radius: 6px;
-          font-size: 14px;
-        }
+    .overview__chart {
+      width: 230px;
+      min-width: 230px;
+      max-width: 230px;
+      padding: 10px;
+      border-left: 1px solid $blockBorder;
+      border-right: 1px solid $blockBorder;
+      .chart__block-title {
+        margin-bottom: 10px;
+        padding: 0 10px 10px 10px;
+        text-align: center;
+        font-weight: 500;
+        border-bottom: 1px dashed rgba(0, 0, 0, 0.2);
+        white-space: nowrap;
       }
     }
-  }
-  .found-program__action-btns {
-    display: flex;
-    align-items: center;
-    margin-top: 10px;
+
+    .overview__actions {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-around;
+      padding: 10px;
+      background: $hiddenBlockBG;
+      .action-btn {
+        padding: 5px;
+        font-size: 20px;
+      }
+    }
   }
 }
 
