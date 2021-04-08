3b546b138d2d791ea5c10138b941850b64ef64e7
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-04-08 16:47:48 +0400

    create base layout for meal plans page

--- pages/meal-planer/meal-plans/index.vue
+++ pages/meal-planer/meal-plans/index.vue
@@ -1,16 +1,49 @@
 <template>
   <div class="meal-plans-page">
-    meal-plans page
+    <app-page-title>Сохраненные рационы</app-page-title>
+    <div class="meal-plans-page__content">
+      <page-info />
+      <div class="filters-and-table">
+        <!-- <sorting-filters />
+        <product-table /> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import AppPageTitle from '@/components/basic/AppPageTitle'
+import PageInfo from '@/components/mealPlans/PageInfo'
+
 export default {
-  layout: 'default'
+  layout: 'default',
+  components: {
+    AppPageTitle,
+    PageInfo
+  }
 }
 </script>
 
 <style lang="scss">
 @import '@/assets/styles/vars.scss';
+
+.meal-plans-page {
+  // border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 80px;
+  padding: 40px;
+  .meal-plans-page__content {
+    // border: 1px solid red;
+    flex: 1 1 auto;
+    display: flex;
+    width: 100%;
+    max-width: 1700px;
+    .filters-and-table {
+      display: flex;
+    }
+  }
+}
 
 </style>
