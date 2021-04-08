0d9fb5abda4eacb6be188de2856f5ac33bf6896d
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-04-08 16:45:14 +0400

    set defaul pecipes

--- store/recipesBook.js
+++ store/recipesBook.js
@@ -1,5 +1,29 @@
 export const state = () => ({
-  recipes: []
+  recipes: [
+    {
+      id: 1,
+      title: 'title',
+      description: 'desc',
+      cookingTimes: '124',
+      products: [
+        {
+          id: 0,
+          title: 'dfhgdfh',
+          weight: 100,
+          protein: 1,
+          fats: 0.5,
+          carb: 45,
+          kkal: 321,
+          category: 'category',
+          favorite: true,
+          userProduct: true,
+          userId: null,
+          weightInRecipe: 9
+        }
+      ],
+      cookingSkill: 2,
+    }
+  ]
 })
 
 export const getters = {
@@ -17,10 +41,10 @@ export const mutations = {
 export const actions = {
   async fetchRecipes ({ commit }) {
     try {
-      const Recipes = await this.$axios.$get('http://localhost:3030/api/recipes-book')
+      // const Recipes = await this.$axios.$get('http://localhost:3030/api/recipes-book')
 
-      commit('setRecipes', Recipes)
-      console.log(Recipes)
+      // commit('setRecipes', Recipes)
+      // console.log(Recipes)
     } catch (e) {
       console.log(e)
     }
