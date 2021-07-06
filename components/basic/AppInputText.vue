b7829452a5fa50655321bb2c525cedf6867481dc
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-07-06 10:11:44 +0400

    add field error notice

--- components/basic/AppInputText.vue
+++ components/basic/AppInputText.vue
@@ -1,17 +1,25 @@
 <template>
-  <input
-    class="input-text"
-    :class="[
-      { 'input-text--small': small },
-      { 'input-text--text-left': textLeft },
-      { 'input-text--text-right': textRight },
-      { 'input-text--text-center': textCenter }
-    ]"
-    type="text"
-    :placeholder="placeholder"
-    @input="changeInputValue()"
-    v-model="inputValue"
-  >
+  <div class="app-input-text">
+    <input
+      class="input"
+      :class="[
+        { 'input--small': small },
+        { 'input--text-left': textLeft },
+        { 'input--text-right': textRight },
+        { 'input--text-center': textCenter },
+        { 'input--error': error && error.enabled }
+      ]"
+      type="text"
+      :placeholder="placeholder"
+      @input="changeInputValue()"
+      v-model="inputValue"
+    >
+
+    <pre
+      v-if="error && error.errorMessage"
+      class="error-message"
+    >{{ error.errorMessage }}</pre>
+  </div>
 </template>
 
 <script>
@@ -22,11 +30,12 @@ export default {
     small: Boolean,
     textLeft: Boolean,
     textRight: Boolean,
-    textCenter: Boolean
+    textCenter: Boolean,
+    error: Object,
   },
   data () {
     return {
-      inputValue: this.value || ''
+      inputValue: this.value
     }
   },
   watch: {
@@ -45,45 +54,76 @@ export default {
 <style lang="scss">
 @import "@/assets/styles/vars.scss";
 
-.input-text {
-  padding: 10px;
-  width: 100%;
-  min-width: 50px;
-  color: $black;
-  font-family: $fontMontserrat;
-  font-size: 16px;
-  border: 1px solid $blockBorder;
-  border-radius: 6px;
-  outline: none;
-  transition: $tr-02;
-}
-.input-text:focus {
-  border: 1px solid $green;
-  transition: $tr-02;
-}
-.input-text::placeholder {
-  opacity: .6;
-  transition: $tr-02;
-}
-.input-text:focus::placeholder {
-  opacity: 0;
-}
+.app-input-text {
+  // border: 1px solid red;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .input {
+    padding: 10px;
+    width: 100%;
+    min-width: 50px;
+    color: $black;
+    font-family: $fontMontserrat;
+    font-size: 16px;
+    border: 1px solid $blockBorder;
+    border-radius: 6px;
+    outline: none;
+    transition: $tr-02;
+  }
+  .input:focus {
+    border: 1px solid $green;
+    transition: $tr-02;
+  }
+  .input::placeholder {
+    opacity: .6;
+    transition: $tr-02;
+  }
+  .input:focus::placeholder {
+    opacity: 0;
+  }
 
-.input-text--small {
-  padding: 5px 10px;
-  color: $black;
-  font-size: 14px;
-}
+  .input--small {
+    padding: 5px 10px;
+    color: $black;
+    font-size: 14px;
+  }
 
-.input-text--text-left {
-  text-align: left;
-}
-.input-text--text-right {
-  text-align: right;
-}
-.input-text--text-center {
-  text-align: center;
-}
+  .input--text-left {
+    text-align: left;
+  }
+  .input--text-right {
+    text-align: right;
+  }
+  .input--text-center {
+    text-align: center;
+  }
 
+  .input--error {
+    border: 1px solid $red !important;
+  }
+
+  .error-message {
+    // border: 1px solid red;
+    position: absolute;
+    top: calc(100% + 10px);
+    padding: 5px 10px;
+    background: $red;
+    color: $white;
+    border-radius: 6px;
+    z-index: 2000;
+  }
+  .error-message::before {
+    position: absolute;
+    content: '';
+    top: -2px;
+    left: 50%;
+    transform: rotate(45deg) translateX(-50%);
+    width: 10px;
+    height: 10px;
+    background: $red;
+  }
+}
 
 </style>
