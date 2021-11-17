2da1c3d287740453e0503bb09f544ecf68139994
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-11-17 11:40:25 +0400

    create component

---
+++ components/basic/AppInfo.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    class="app-info"
+    :class="[
+      { 'app-info--success': success },
+      { 'app-info--warning': warning },
+      { 'app-info--info': info },
+      { 'app-info--alert': alert },
+      { 'app-info--small': small },
+      { 'app-info--big': big }
+    ]"
+  >
+    <i
+      class="app-info__icon"
+      :class="[
+        { 'ti-check-box': success },
+        { 'ti-na': warning },
+        { 'ti-info-alt': info },
+        { 'ti-alert': alert },
+        { 'ti-pencil-alt': !success && !warning && !info && !alert },
+      ]"
+    ></i>
+    <p class="app-info__text">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    success: Boolean,
+    warning: Boolean,
+    info: Boolean,
+    alert: Boolean,
+    text: String,
+    small: Boolean,
+    big: Boolean
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/vars.scss";
+
+.app-info {
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+  background: $gray-light;
+  color: $gray-dark;
+  border-radius: 6px;
+  .app-info__icon {
+    margin-right: 10px;
+    font-size: 20px;
+  }
+  .app-info__text {
+    font-size: 16px;
+  }
+}
+
+.app-info--success {
+  background: $green;
+  color: $white;
+}
+.app-info--warning {
+  background: $yellow;
+  color: $black;
+}
+.app-info--info {
+  background: $blue;
+  color: $white;
+}
+.app-info--alert {
+  background: $red;
+  color: $white;
+}
+
+.app-info--small {
+  padding: 8px 15px;
+  .app-info__icon {
+    margin-right: 10px;
+    font-size: 18px;
+  }
+  .app-info__text {
+    font-size: 14px;
+  }
+}
+.app-info--big {
+  padding: 15px;
+  .app-info__icon {
+    margin-right: 10px;
+    font-size: 20px;
+  }
+  .app-info__text {
+    font-size: 18px;
+  }
+}
+
+</style>
