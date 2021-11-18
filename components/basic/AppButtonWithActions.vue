595fdd54cb4ac1e11a4d283f7610563de685b2d0
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-11-18 10:55:03 +0400

    refoctoring

--- components/basic/AppButtonWithActions.vue
+++ components/basic/AppButtonWithActions.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="btn-with-actions">
-    <button class="btn-with-actions__btn" @click="toggleVisibility($event)">
-      <i class="ti-more btn__icon"></i>
-      <ul
-        v-show="listOpened"
-        ref="actionsList"
-        class="btn-with-actions__actions-list"
-        :class="[
-          {'btn-with-actions__actions-list--right': rightSide},
-          {'btn-with-actions__actions-list--left': leftSide}
-        ]"
+  <button class="btn-with-actions" @click="toggleVisibility($event)">
+    <i class="ti-more btn-with-actions__icon"></i>
+    <ul
+      v-show="listOpened"
+      ref="actionsList"
+      class="btn-with-actions__actions-list"
+      :class="[
+        {'btn-with-actions__actions-list--right': rightSide},
+        {'btn-with-actions__actions-list--left': leftSide}
+      ]"
+    >
+      <li
+        v-for="(action, index) in actions"
+        :key="index"
+        class="actions-list__item"
+        @click="actionHandler({action, params})"
       >
-        <li
-          v-for="(action, index) in actions"
-          :key="index"
-          class="actions-list__item"
-          @click="actionHandler({action, params})"
-        >
-          <p class="item__text">{{ action }}</p>
-        </li>
-      </ul>
-    </button>
-  </div>
+        <p class="item__text">{{ action }}</p>
+      </li>
+    </ul>
+  </button>
 </template>
 
 <script>
@@ -81,88 +79,80 @@ export default {
 
 .btn-with-actions {
   // border: 1px solid red;
-
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
-
-  .btn-with-actions__btn {
-    // border: 1px solid red;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 0;
-    background: transparent;
-    color: $black;
-    font-family: $fontMontserrat;
+  padding: 0;
+  background: transparent;
+  color: $black;
+  font-family: $fontMontserrat;
+  font-size: 16px;
+  border: none;
+  outline: none;
+  .btn-with-actions__icon {
+    padding: 5px;
     font-size: 16px;
-    border: none;
-    outline: none;
-    .btn__icon {
-      padding: 5px;
-      font-size: 16px;
-      cursor: pointer;
-      transition: $tr-01;
-    }
-    .btn-with-actions__actions-list {
+    cursor: pointer;
+    transition: $tr-01;
+  }
+  .btn-with-actions__actions-list {
+    // border: 1px solid red;
+    position: absolute;
+    top: 30px;
+    background: $white;
+    border: 1px solid $blockBorder;
+    border-radius: 6px;
+    box-shadow: $boxShadow;
+    z-index: 2;
+    .actions-list__item {
       // border: 1px solid red;
-      position: absolute;
-      top: 30px;
-      background: $white;
-      border: 1px solid $blockBorder;
-      border-radius: 6px;
-      box-shadow: $boxShadow;
-      z-index: 2;
-      .actions-list__item {
-        // border: 1px solid red;
-        position: relative;
-        padding: 10px 20px;
-        border-bottom: 1px solid $blockBorder;
-        cursor: pointer;
-        .item__text {
-          text-align: center;
-          font-size: 14px;
-          white-space: nowrap;
-        }
-      }
-      .actions-list__item:last-child {
-        border: none;
-      }
-      .actions-list__item:hover {
-        color: $green;
+      position: relative;
+      padding: 10px 20px;
+      border-bottom: 1px solid $blockBorder;
+      cursor: pointer;
+      .item__text {
+        text-align: center;
+        font-size: 14px;
+        white-space: nowrap;
       }
     }
-    .btn-with-actions__actions-list--right {
-      left: -10px;
+    .actions-list__item:last-child {
+      border: none;
     }
-    .btn-with-actions__actions-list--left {
-      right: -10px;
+    .actions-list__item:hover {
+      color: $green;
     }
-    .btn-with-actions__actions-list--left:after {
-      position: absolute;
-      content: '';
-      top: -7px;
-      right: 14px;
-      width: 14px;
-      height: 14px;
-      background: $white;
-      border-top: 1px solid $blockBorder;
-      border-left: 1px solid $blockBorder;
-      transform: rotate(45deg);
-    }
-    .btn-with-actions__actions-list--right:after {
-      position: absolute;
-      content: '';
-      top: -7px;
-      left: 15px;
-      width: 14px;
-      height: 14px;
-      background: $white;
-      border-top: 1px solid $blockBorder;
-      border-left: 1px solid $blockBorder;
-      transform: rotate(45deg);
-    }
+  }
+  .btn-with-actions__actions-list--right {
+    left: -10px;
+  }
+  .btn-with-actions__actions-list--left {
+    right: -10px;
+  }
+  .btn-with-actions__actions-list--left:after {
+    position: absolute;
+    content: '';
+    top: -7px;
+    right: 14px;
+    width: 14px;
+    height: 14px;
+    background: $white;
+    border-top: 1px solid $blockBorder;
+    border-left: 1px solid $blockBorder;
+    transform: rotate(45deg);
+  }
+  .btn-with-actions__actions-list--right:after {
+    position: absolute;
+    content: '';
+    top: -7px;
+    left: 15px;
+    width: 14px;
+    height: 14px;
+    background: $white;
+    border-top: 1px solid $blockBorder;
+    border-left: 1px solid $blockBorder;
+    transform: rotate(45deg);
   }
 }
 
