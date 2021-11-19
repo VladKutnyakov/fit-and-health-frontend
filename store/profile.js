ab554f4c5dc91a0bfe258753da7bda740c21c3bf
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-11-19 13:54:46 +0400

    update action url

--- store/profile.js
+++ store/profile.js
@@ -32,7 +32,7 @@ export const actions = {
   async fetchProfileInfo ({ commit }, userId) {
     try {
       // console.log(`${process.env.BASE_URL}/api/profile/${userId}`)
-      const response = await this.$axios.$get(`${process.env.BASE_URL}/api/profile/${userId}`)
+      const response = await this.$axios.$get(`${process.env.BASE_URL}/api/profiles/${userId}`)
 
       if (response.updatedToken) {
         this.commit('auth/setToken', response.updatedToken)
