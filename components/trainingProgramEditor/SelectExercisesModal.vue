508791807cddbd777a93188c30677c0f6f787ed4
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2022-02-08 14:32:53 +0400

    update layout

--- components/trainingProgramEditor/SelectExercisesModal.vue
+++ components/trainingProgramEditor/SelectExercisesModal.vue
@@ -6,8 +6,43 @@
     @close="toggleModalVisibility({modal: 'selectExercisesModalActive', condition: false})"
   >
     <template v-slot:modalContent>
-      <div class="content__exercises">
-        exercises
+      <div class="content__exercises-list">
+        <div class="search">
+          <app-search-block small />
+        </div>
+
+        <div class="categories">
+          <ul class="categories__list">
+            <li class="list__item" v-for="(item, index) in muscleGroups" :key="index">
+              <app-accordion :isOpened="muscleGroupIsOpened">
+                <template v-slot:accordionHeader>
+                  <div class="item__header" @click="muscleGroupIsOpened = !muscleGroupIsOpened">
+                    <div class="preview-image">
+                      <img :src="item.previewImage" :alt="item.title" class="image">
+                    </div>
+                    <div class="header__title">
+                      <p class="title__text">{{ item.title }}</p>
+                      <p class="title__exercises-count">Кол-во упражнений {{ item.exercises.length }}</p>
+                    </div>
+                    <i v-if="!muscleGroupIsOpened" class="ti-angle-double-down header__icon"></i>
+                    <i v-if="muscleGroupIsOpened" class="ti-angle-double-up header__icon"></i>
+                  </div>
+                </template>
+                <template v-slot:accordionHiddenContent>
+                  <ul class="item__exercises-list">
+                    <!-- <exercise
+                      v-for="(item, index) in muscleGroup.exercises"
+                      :key="index"
+                      :item="item"
+                      :muscleGroup="muscleGroup"
+                      class="exercises-list__item"
+                    /> -->
+                  </ul>
+                </template>
+              </app-accordion>
+            </li>
+          </ul>
+        </div>
       </div>
     </template>
     <template v-slot:modalFooter>
@@ -30,21 +65,28 @@
 <script>
 import { mapState, mapMutations } from 'vuex'
 import AppModal from "@/components/basic/AppModal"
-// import AppInputText from "@/components/basic/AppInputText"
-// import AppInputCheckbox from "@/components/basic/AppInputCheckbox"
-// import AppSelect from "@/components/basic/AppSelect"
+import AppSearchBlock from '@/components/basic/AppSearchBlock'
+import AppAccordion from '@/components/basic/AppAccordion'
 import AppButton from "@/components/basic/AppButton"
 
 export default {
   components: {
     AppModal,
-    // AppInputText,
-    // AppInputCheckbox,
-    // AppSelect,
+    AppSearchBlock,
+    AppAccordion,
     AppButton
   },
   data() {
-    return {}
+    return {
+      muscleGroupIsOpened: true,
+      muscleGroups: [
+        {
+          title: '1',
+          previewImage: null,
+          exercises: []
+        }
+      ]
+    }
   },
   computed: {
     ...mapState({
