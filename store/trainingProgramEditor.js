00601e89e6bba4bfb9346e288d674eb9af1df86e
Korobov Dmitry <redfrag@yandex.ru>
2022-03-08 20:57:24 +0400

    update layout

--- store/trainingProgramEditor.js
+++ store/trainingProgramEditor.js
@@ -100,6 +100,7 @@ export const mutations = {
       if (state.selectedTrainingDay !== 0) {
         state.selectedTrainingDay -= 1
       }
+      window.scrollTo(0, 0)
     }
   },
   setExercisesList (state, payload) {
