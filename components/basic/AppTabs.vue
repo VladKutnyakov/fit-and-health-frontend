4783224dbce51ffc9e6393db77393cc38d3a4d15
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2022-03-09 13:57:35 +0400

    add tabs component

---
+++ components/basic/AppTabs.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app-tabs">
+    <ul class="app-tabs__tab-list">
+      <li
+        v-for="(item, index) in tabList"
+        :key="index"
+        class="tab-list__item"
+      >
+        <p>{{ item.title }}</p>
+      </li>
+    </ul>
+
+    <component
+      :is="currentTabComponent"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tabList: Array,
+  },
+  computed: {
+    currentTabComponent () {
+      const activeTab = this.tabList.find(element => element.isActive)
+
+      return activeTab ? activeTab.component : null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/vars.scss';
+
+.app-tabs {}
+
+</style>
