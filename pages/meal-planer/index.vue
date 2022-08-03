944fedb514154c6ce8b2800cf6b61b5c93c57175
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2022-08-03 10:12:41 +0400

    update

--- pages/meal-planer/index.vue
+++ pages/meal-planer/index.vue
@@ -59,6 +59,8 @@ export default {
   },
   computed: {
     ...mapState({
+      mealParts: state => state.mealPlaner.mealPlanerInfo.fields.mealParts,
+      selectedMealPart: state => state.mealPlaner.selectedMealPart,
       selectProductModalActive: state => state.mealPlaner.selectProductModalActive,
     }),
   },
@@ -67,8 +69,28 @@ export default {
       setModalVisibility: 'mealPlaner/setModalVisibility',
     }),
     selectProduct ($event) {
-      // console.log('selectProduct', $event)
-      this.$store.commit('mealPlaner/addProductInMealPart', JSON.parse(JSON.stringify($event)))
+      const products = this.mealParts[this.selectedMealPart].mealPartProducts
+
+      let canBeAdded = true
+      for (let i = 0; i < products.length; i++) {
+        if (products[i].product.id === $event.id) {
+          canBeAdded = false
+
+          const notice = {
+            id: Date.now(),
+            type: 'warning',
+            message: 'Продукт уже добавлен в выбранный прием пищи.',
+            timeToShow: 5000,
+          }
+          this.$store.commit('notifications/addNewNotice', notice)
+
+          break
+        }
+      }
+
+      if (canBeAdded) {
+        this.$store.commit('mealPlaner/addProductInMealPart', JSON.parse(JSON.stringify($event)))
+      }
     },
   },
 }
